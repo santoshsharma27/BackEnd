a4457e11071706b3add775a8b5aaa6098f7e8fdc
santoshsharma27 <santosh37kr@gmail.com>
2024-09-15 23:58:27 +0530

    Leaning BackEnd

--- Natours/app.js
+++ Natours/app.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const fs = require("fs");
-const morgan = require("morgan");
 
 const app = express();
 
-// 1) MIDDLEWARES
-app.use(morgan("dev"));
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
